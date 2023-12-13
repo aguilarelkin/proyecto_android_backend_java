4b41038b802d7e7d4a970ec0d027e26836769f53
Elkin Aguilar <aguilar.elkin99@gmail.com>
2023-12-12 22:00:45 -0500

    create unit-testing controller

--- src/test/java/com/anexang/app/controllers/EmployeContollerTest.java
+++ src/test/java/com/anexang/app/controllers/EmployeContollerTest.java
@@ -1,8 +1,36 @@
 package com.anexang.app.controllers;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+
+import com.anexang.app.Data;
+import com.anexang.app.domain.service.IEmployeService;
+
+@ExtendWith(MockitoExtension.class)
 public class EmployeContollerTest {
+
+    @InjectMocks
+    private EmployeContoller contoller;
+
+    @InjectMocks
+    private Data dataEmploye;
+
+    @Mock
+    private BindingResult result;
+
+    @Mock
+    private IEmployeService employeService;
+
     @Test
     void testCreateEmploye() {
 
@@ -20,7 +48,11 @@ public class EmployeContollerTest {
 
     @Test
     void testGetEmployes() {
-
+        when(employeService.getEmployes()).thenReturn(dataEmploye.getData());
+        
+        ResponseEntity<?> response = contoller.getEmployes();
+        verify(employeService, times(1)).getEmployes();
+        assertTrue(response.toString().contains("200"));
     }
 
     @Test
